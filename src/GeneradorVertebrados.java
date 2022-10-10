b98bfc27130a3a9bbacf9e5ecfb79a91710733a8
Jose Andres Casanova <joseandrescazanova@gmail.com>
2022-10-10 16:26:46 -0500

    Se realizado la eliminacion de la lista de animales a el animal 'Tiburón' y se agrego  'Perro'.

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,9 +1,13 @@
 public class GeneradorVertebrados {
+    /**
+     * @return
+     */
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        //miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
+        miVertebrados[2] = new Vertebrado("Perro", "Mamifero"); 
         
         return miVertebrados;
     }
